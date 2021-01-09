11ce284acf33e3f36047ca21028bedc5662e981e
adirins <andris.dirins@gmail.com>
2021-01-09 16:34:40 +0200

    home page first info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-for="item in info" :key="item.id">
+      {{item.title}}
+    </div>
   </div>
 </template>
 
