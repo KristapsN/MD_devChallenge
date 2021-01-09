a257fa268c81f4d78d04f57ac14a977ec9324588
adirins <andris.dirins@gmail.com>
2021-01-09 14:46:25 +0200

    added axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/article">Article</router-link>
+    </div>
   <router-view/>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    const accessPoint = 'https://cors-anywhere.herokuapp.com';
+    const url = 'https://jobs.github.com/positions.json';
+    return axios
+      .get(`${accessPoint}/${url}?page=${1}`)
+      .then((response) => {
+        this.info = response.data;
+      });
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
