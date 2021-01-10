9227916f044bdb09c068ad986a1aa3f6c256f86e
adirins <andris.dirins@gmail.com>
2021-01-10 16:35:59 +0200

    added useParams

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: '/article',
+    path: '/article/:id',
     name: 'Article',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="article">
-    <h1>This is an article page</h1>
+  <div class="home">
+    {{ $route.params.id }}
+    <div v-for="item in info" :key="item.id">
+      <div v-if=" $route.params.id === item.id">
+        <h1>This is an article page with title: {{ item.title }}</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    const accessPoint = 'https://cors-anywhere.herokuapp.com';
+    const url = 'https://jobs.github.com/positions.json';
+    return axios.get(`${accessPoint}/${url}?page=${1}`).then((response) => {
+      this.info = response.data;
+    });
+  },
+};
+</script>
