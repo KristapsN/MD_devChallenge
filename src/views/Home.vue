a490880bcfc991ef23f901c7497faab4a1da438a
KristapsN <kristaps.nogobods@gmail.com>
2021-01-11 12:28:20 +0200

    Conflict solved

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,25 +17,6 @@
         Full time: {{ selectedFullTime }}
       </div>
       <div class="col-xs-9">
-
-        <div v-for="job in evenNumbers" :key="job.id">
-          <router-link :to="'/article/' + job.id">
-            <JobCard
-              :image="job.company_logo"
-              :company="job.company"
-              :vacancy="job.title"
-              :position="job.type"
-              :location="job.location"
-              :date="job.created_at"
-            />
-          </router-link>
-        </div>
-        <div class="row">
-          <div class="col-xs-12">
-            <div  v-for="(job, index) in jobs" :key="job.id">
-            <PageSelection :selectedPage="index" @selectedPage="chosePage" />
-            </div>
-
         <section v-if="errored">
           <p>
             We're sorry,
@@ -46,13 +27,11 @@
         <section v-else>
           <div v-if="loading">
             Loading...
-          </div>  
-         </section>
           </div>
           <div v-else class="row">
             <div class="col-xs-12">
-              <div v-for="job in jobs" :key="job.id">
-                <router-link :to="'/article/'+job.id">
+               <div v-for="job in evenNumbers" :key="job.id">
+                 <router-link :to="'/article/' + job.id">
                   <JobCard
                     :image="job.company_logo"
                     :company="job.company"
@@ -61,15 +40,15 @@
                     :location="job.location"
                     :date="job.created_at"
                   />
-                </router-link>
-              </div>
+                 </router-link>
+                </div>
             </div>
-            <div class="row">
-              <div class="col-xs-12">
-                <PageSelection/>
-              </div>
+          <div class="col-xs-12">
+            <div  v-for="(job, index) in jobs" :key="job.id">
+             <PageSelection :selectedPage="index" @selectedPage="chosePage" />
             </div>
           </div>
+        </div>
         </section>
       </div>
     </div>
@@ -109,17 +88,19 @@ export default defineComponent({
   mounted() {
     const accessPoint = 'https://cors-anywhere.herokuapp.com';
     const url = 'https://jobs.github.com/positions.json';
-    return axios
-      .get(`${accessPoint}/${url}?page=${1}`)
-      .then((response) => {
-        this.jobs = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        this.errored = true;
-      })
-      // eslint-disable-next-line no-return-assign
-      .finally((): boolean => this.loading = false);
+    return (
+      axios
+        .get(`${accessPoint}/${url}?page=${1}`)
+        .then((response) => {
+          this.jobs = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        // eslint-disable-next-line no-return-assign
+        .finally((): boolean => (this.loading = false))
+    );
   },
   computed: {
     evenNumbers() {
